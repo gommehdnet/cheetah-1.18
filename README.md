3809269826c8db5fd3c0151eefe0e4b7ea08891f
lukas81298 <lukas81298@gommehd.net>
2021-12-16 18:15:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# GommeHD.net Cheetah 1.18
+
+This is a fork of PaperSpigot tailored for the use at GommeHD.net
+
+## How to build
+- Clone this repository
+- Open a shell (e.g. using Git Bash on Windows)
+- Run `./gradlew applyPatches` to apply the patches
+- Run `./gradlew build` to build the jar files
+- Run `./gradlew shadowJar` to create the final jar file
+- The final jar file can be found in `Cheetah-Server/build/libs/Cheetah-Server-reobf.jar`
+
+## How to use the API with Maven?
+- Run `./gradlew publishToMavenLocal`
+- Add the following maven dependency:
+  ``<dependency>
+  <groupId>net.gommehd.cheetah</groupId>
+  <artifactId>cheetah-server</artifactId>
+  <version>1.18-R0.1-SNAPSHOT</version>
+  </dependency>``
+
+## How to apply changes
+- First apply the patches using `./gradlew applyPatches`
+- Perform the changes to the code
+- If you are a missing file, you can import it from NMS. For this, add a new line to `build-data/dev-imports.txt`, e.g. `minecraft net.minecraft.world.level.entity.LevelEntityGetterAdapter` to import the LevelEntityGetterAdapter. Then apply the patches again (`./gradlew applyPatches`). Keep in mind that all changes to the code will get LOST by this!
+- Once all changes have been applied, switch to the Subproject in your Shell (`cd Cheetah-Server` or `cd Cheetah-API`) and run `git add .`. Then, commit your changes using `git commit -m "NAME OF PATCH"`
+- Switch back to the project root and rebuild the patches: `./gradlew rebuildPatches`
+- Commit the changes to the `patches/` folder
+
+## Contributing
+Only pull requests of the GommeHD.net dev team members are accepted. If you want to add a patch others could benefit from as well, consider submitting it to the upstream (https://github.com/PaperMC/Paper).
---
+++ build.gradle.kts
@@ -0,0 +1,113 @@
+import io.papermc.paperweight.util.constants.*
+
+plugins {
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.3.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.7.0:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
+    paperclip("io.papermc:paperclip:3.0.2")
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":cheetah-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+    usePaperUpstream(providers.gradleProperty("paperRef")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("cheetah-api"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("cheetah-server"))
+        }
+    }
+}
+
+//
+// Everything below here is optional if you don't care about publishing API or dev bundles to your repository
+//
+
+tasks.generateDevelopmentBundle {
+    apiCoordinates.set("com.example.paperfork:cheetah-api")
+    mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
+    libraryRepositories.set(
+        listOf(
+            "https://repo.maven.apache.org/maven2/",
+            "https://libraries.minecraft.net/",
+            "https://papermc.io/repo/repository/maven-public/",
+            "https://maven.quiltmc.org/repository/release/",
+			"https://repo.gomme.dev/repository/public/",
+        )
+    )
+}
+
+allprojects {
+    // Publishing API:
+    // ./gradlew :Cheetah-API:publish[ToMavenLocal]
+    publishing {
+        repositories {
+            maven {
+                name = "myRepoSnapshots"
+                url = uri("https://my.repo/")
+                // See Gradle docs for how to provide credentials to PasswordCredentials
+                // https://docs.gradle.org/current/samples/sample_publishing_credentials.html
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+}
+
+publishing {
+    // Publishing dev bundle:
+    // ./gradlew publishDevBundlePublicationTo(MavenLocal|MyRepoSnapshotsRepository) -PpublishDevBundle
+    if (project.hasProperty("publishDevBundle")) {
+        publications.create<MavenPublication>("devBundle") {
+            artifact(tasks.generateDevelopmentBundle) {
+                artifactId = "dev-bundle"
+            }
+        }
+    }
+}
